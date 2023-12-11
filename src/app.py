c9d63c5fa20f57cba88f5043c7775bd60fe8c71a
yasminingah <77464994+yasminingah@users.noreply.github.com>
2023-12-11 16:25:58 -0700

    Add existing file

--- src/app.py
+++ src/app.py
@@ -1,20 +1,16 @@
 #!/usr/bin/env python3
-#<link rel="stylesheet" type="text/css" href="style.css">
-
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return '''
-     <form action="/echo_user_input" method="POST">
-         <label for="user_input">Enter your expense:</label>
-         <input name="user_input" id="user_input" type="text">
-         <input type="submit" value="Submit!">
-     </form>
-     '''
+    return render_template("styles.html")
+
 @app.route("/echo_user_input", methods=["POST"])
 def echo_input():
     input_text = request.form.get("user_input", "")
     return "You entered: " + input_text
+
+if __name__ == '__main__':
+    app.run()
